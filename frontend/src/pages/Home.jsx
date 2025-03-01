f4d0b6417a3edd969c7aa0cb22b4291fb9ebdcdb
Sushantsth21 <sushant.shrestha003@gmail.com>
2025-03-01 15:46:30 -0500

    map in place

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import HomeComponent from '../components/HomeComponent';
+import FinalMap from '../maps/finalMap';
 
 function Home() {
   return (
     <div>
       <HomeComponent />
-      
+      <FinalMap />
     </div>
   );
 }
