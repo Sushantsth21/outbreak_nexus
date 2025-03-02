2553a9c010c3f51f60171b7aefd489aa4d492e62
Mithlesh Sah <117466084+Mithsah1325@users.noreply.github.com>
2025-03-02 04:14:35 -0500

    generate report fixed

--- frontend/src/components/SecondaryComponent.jsx
+++ frontend/src/components/SecondaryComponent.jsx
@@ -4,7 +4,7 @@ import Remark from "./Remark";
 import AboutUs from "../pages/AboutUs";
 
 const Modal = ({ title, onClose, children }) => (
-  <div className="fixed top-0 left-0 right-0 bottom-0 bg-gray-900 bg-opacity-50 flex justify-center items-center z-10">
+  <div className="x-15 fixed top-0 left-0 right-0 bottom-0 bg-gray-900 bg-opacity-50 flex justify-center items-center z-10">
     <div className="bg-white p-8 rounded-lg max-w-lg w-full shadow-lg transform transition duration-300 hover:scale-105">
       <h2 className="text-2xl font-semibold mb-4 text-center text-blue-600">{title}</h2>
       {children}
